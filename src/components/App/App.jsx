a495c6c8e92e5d30bf388260512c3357dbb6a229
Matt Javier <matthewrjavier@gmail.com>
2021-01-21 20:14:04 -0800

    start, added Flight component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,8 +11,13 @@ const App = () => (
   <div className={getClassName('App')}>
     <Header />
     <main className={getClassName('App__main')}>
-      <BpkText tagName="p">Over to you...</BpkText>
+      {/* <BpkText tagName="p">Over to you...</BpkText> */}
       {/* TODO: Add a component to display results here */}
+      
+      {/* List of FLights */}
+      {/* each Card will be an itinerary  */}
+      {/* each itinerary will have a 2 Legs */}
+      {/* each Leg is a Flight */}
     </main>
   </div>
 );
